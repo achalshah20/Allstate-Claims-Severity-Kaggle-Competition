fd6a2da491dfadd4c5ca93b8ec2089d071586236
Achal <achalshah20@gmail.com>
2016-11-05 20:54:51 -0400

    XGB with LB: 1126

---
+++ data_preprocessing.py
@@ -0,0 +1,34 @@
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+
+def onehotencoding(data,catFeaturesList_,le,maxCatValues):
+
+    cats = []
+    for catFeature in catFeaturesList_:
+        labels = data[catFeature].unique()
+        if(len(labels) < maxCatValues):
+            features = pd.get_dummies(data[catFeature])
+            features.columns = catFeature + " = " + features.columns
+
+            #print(catFeature)
+            data = data.drop(catFeature,axis=1)
+            data = pd.concat([data,features.astype(int)],axis=1)
+        else:
+            le.fit(labels)
+            data[catFeature] = le.transform(data[catFeature])
+    return(data)
+
+def get_cat_features(data):
+    catFeatureslist = []
+    for colName,x in data.iloc[1,:].iteritems():
+        if(str(x).isalpha()):
+            catFeatureslist.append(colName)
+    return(catFeatureslist)
+
+def transform_cat_features(data,catFList_):
+    for cf1 in catFList_:
+        le = LabelEncoder()
+        le.fit(data[cf1].unique())
+        data[cf1] = le.transform(data[cf1])
+    return(data)
